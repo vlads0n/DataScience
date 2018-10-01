cc58b56352efeb1e88ad603db2f88253a36ebc69
vlads0n <19vlad_son90@bk.ru>
2018-10-02 00:15:02 +0300

    Counting sort

--- Test.py
+++ Test.py
@@ -28,7 +28,24 @@ def quick_sort(a):
         return quick_sort(less) + equal + quick_sort(greater)
     else:
         return a
-    
+
+def counting_sort(a):
+    counts = [0 for i in range(max(a) + 1)]
+
+    for el in a:
+        counts[el] += 1
+
+    for index in range(1, len(counts)):
+        counts[index] = counts[index - 1] + counts[index]
+
+    L = [0 for loop in range(len(a) + 1)]
+    for el in a:
+        index = counts[el] - 1
+        L[index] = el
+        counts[el] -= 1
+
+    return L
+
 def insertion_time(n):
     a = [i for i in range(n)]
     shuffle(a)
@@ -43,8 +60,14 @@ def quick_sort_time(n):
     quick_sort(a)
     return (datetime.now() - start).microseconds
 
-plt.plot([10, 100, 1000, 10000],
-         [insertion_time(10), insertion_time(100), insertion_time(1000), insertion_time(10000)])
-plt.plot([10, 100, 1000, 10000],
-         [quick_sort_time(10), quick_sort_time(100), quick_sort_time(1000), quick_sort_time(10000)])
+def counting_sort_time(n):
+    a = [i for i in range(n)]
+    shuffle(a)
+    start = datetime.now()
+    counting_sort(a)
+    return (datetime.now() - start).microseconds
+
+plt.plot([10, 100, 1000, 10000], [insertion_time(10), insertion_time(100), insertion_time(1000), insertion_time(10000)])
+plt.plot([10, 100, 1000, 10000], [quick_sort_time(10), quick_sort_time(100), quick_sort_time(1000), quick_sort_time(10000)])
+plt.plot([10, 100, 1000, 10000], [counting_sort_time(10), counting_sort_time(100), counting_sort_time(1000), counting_sort_time(10000)])
 plt.show()
